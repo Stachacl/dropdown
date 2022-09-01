20797fc91317219e5163316aec0cd5332d76dec6
Stacha Clemens <stacha@192-168-1-107.tpgi.com.au>
2022-09-01 20:31:10 +1000

    Updates

---
+++ precache-manifest.6f6e539bfc80140f2bfd56ecfd446274.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63322a56e7e87826264346840ed22496",
+    "url": "/dropdown/index.html"
+  },
+  {
+    "revision": "4a713d9f13c0ed6d67e2",
+    "url": "/dropdown/static/css/main.acfd67e1.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/dropdown/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dropdown/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a713d9f13c0ed6d67e2",
+    "url": "/dropdown/static/js/main.70a67dcc.chunk.js"
+  },
+  {
+    "revision": "26efef7da6d23e80b28c",
+    "url": "/dropdown/static/js/runtime-main.c27997e4.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/dropdown/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/dropdown/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/dropdown/static/media/phone.58d7e3d6.svg"
+  }
+]);
